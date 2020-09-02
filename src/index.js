07ad1167ba7ec9457f583de363779b7085b92596
Antonio Perez <aperez2541@gmail.com>
2020-09-02 15:22:22 -0700

    feat(index): add 50mb limit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const pdfService = require('./services/pdf-maker.service');
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded());
 
 app.get("/", async (req, res) => {
