0ce00b88d181f9cab1885c4cd099f15053b6e601
Antonio Perez <aperez2541@gmail.com>
2021-04-04 23:14:20 -0700

    feat(index): add cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@
 const gisService = require('./services/gis.service');
 const pdfService = require('./services/pdf-maker.service');
 const express = require("express");
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded());
