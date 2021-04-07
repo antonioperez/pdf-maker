fcb1b7244d29e492aee66b5bbaeb8bf099e97984
Antonio Perez <aperez2541@gmail.com>
2021-04-07 00:29:16 -0700

    test(services): add local water districts data

--- src/services/gis.service.js
+++ src/services/gis.service.js
@@ -8,14 +8,13 @@ module.exports = {
 
 const cacheKey = 'californiaWaterBoundaries';
 const waterCache = new cacheService();
-
 async function getCaliforniaWaterBoundaries() {
 	const cachedWaterData = waterCache.get(cacheKey);
 	if (cachedWaterData) {
 		return Promise.resolve(cachedWaterData);
 	}
 
-	const caWaterData = await getCaliforniaWaterBoundariesData().catch(() => {});
+	const caWaterData = getCaliforniaWaterBoundariesData();
 
 	if (caWaterData) {
 		const monthInMinutes = 43800;
@@ -26,13 +25,5 @@ async function getCaliforniaWaterBoundaries() {
 }
 
 function getCaliforniaWaterBoundariesData(){
-	const url = 'https://gis.water.ca.gov/arcgis/rest/services/Boundaries/i03_WaterDistricts/FeatureServer/0/query?where=1%3D1&outFields=AGENCYNAME,AGENCYUNIQUEID&outSR=4326&f=geojson';
-
-	return axios.get(url)
-		.then(response => response.data)
-		.catch(error => {
-			const errorResponse = error && error.response && error.response.data;
-
-			return Promise.reject(errorResponse || error);
-		});
+	return require('./ca-water-districts.json');
 }
