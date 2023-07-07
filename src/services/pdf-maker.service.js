893c0a28b2a087a27e4f2efa904d88621d108bef
Antonio Perez <aperez2541@gmail.com>
2023-07-07 14:37:30 -0700

    test

--- src/services/pdf-maker.service.js
+++ src/services/pdf-maker.service.js
@@ -1,6 +1,5 @@
 "use strict";
 const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
 
 module.exports = {
 	buildBlobFromHtml,
@@ -8,11 +7,9 @@ module.exports = {
 
 async function buildBlobFromHtml(htmlString) {
 	const browser = await puppeteer.launch({
-		executablePath: await chromium.executablePath(),
 		headless: true,
 		ignoreHTTPSErrors: true,
-		defaultViewport: chromium.defaultViewport,
-		args: [...chromium.args, "--hide-scrollbars", "--disable-web-security", "--no-sandbox", "--disable-setuid-sandbox"],
+		args: ["--hide-scrollbars", "--disable-web-security", "--no-sandbox", "--disable-setuid-sandbox"],
 	});
 
 	const page = await browser.newPage();
