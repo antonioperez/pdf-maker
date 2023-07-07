cd411d487bf62710275fd6d71d2ce3db533ffa1f
Antonio Perez <aperez2541@gmail.com>
2023-07-07 12:19:40 -0700

    Update yarn.sh

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -2,3 +2,6 @@
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
 sudo curl https://intoli.com/install-google-chrome.sh | bash
+
+sudo amazon-linux-extras install epel -y
+sudo yum install -y chromium
