e144e695bae226fdb6f1edb0ade86f88a3f4c9db
Antonio Perez <aperez2541@gmail.com>
2023-11-13 11:53:19 -0800

    test

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -2,4 +2,4 @@
 
 sudo apt install chromium-browser chromium-codecs-ffmpeg
 sudo amazon-linux-extras install epel -y
-curl https://intoli.com/install-google-chrome.sh | bash
+curl https://gist.githubusercontent.com/rkgrep/5f2e41bb224f6696694d46f2b7058aaa/raw/eba83fd621898478fb12dc8f19fb5e5945b9da25/install-google-chrome.sh | bash
