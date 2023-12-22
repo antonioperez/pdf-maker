2dc40dd62a2120450b7d1e94dc35da446f63c355
Antonio Perez <antonioperez@users.noreply.github.com>
2023-12-22 00:24:30 -0800

    Update pdf-maker.service.js

--- src/services/pdf-maker.service.js
+++ src/services/pdf-maker.service.js
@@ -23,9 +23,9 @@ async function buildBlobFromHtml(htmlString) {
 	});
 
 	const page = await browser.newPage();
-	await page.setUserAgent(
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-	);
+	// await page.setUserAgent(
+	// 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+	// );
 	await page.setContent(htmlString, { waitUntil: "networkidle0" });
 	//await page.goto('data:text/html,' + htmlString, { waitUntil: 'networkidle0' });
 
