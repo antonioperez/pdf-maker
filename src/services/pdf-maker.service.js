128bdee9104c95a939da87d31dcf6de98cf38890
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2024-06-17 12:00:43 -0700

    Update pdf-maker.service.js

--- src/services/pdf-maker.service.js
+++ src/services/pdf-maker.service.js
@@ -7,6 +7,7 @@ module.exports = {
 };
 
 async function buildBlobFromHtml(htmlString) {
+	console.log("htmlString", htmlString);
 	// const browser = await puppeteer.launch({
 	// 	executablePath: "/usr/bin/google-chrome-stable",
 	// 	headless: true,
