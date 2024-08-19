dacd4c7e13edbe26319b2123e694540483c51424
Antonio Perez <3781123+antonioperez@users.noreply.github.com>
2024-08-18 19:16:44 -0700

    add --font-render-hinting=medium

--- src/services/pdf-maker.service.js
+++ src/services/pdf-maker.service.js
@@ -30,7 +30,7 @@ const puppeteerLaunch = async () => {
 
 	browser = await puppeteer.launch({
 		executablePath: await chromium.executablePath(),
-		args: [...chromium.args, '--disable-gpu'],
+		args: [...chromium.args, '--disable-gpu','--font-render-hinting=medium'],
 		headless: chromium.headless,
 		ignoreHTTPSErrors: true,
 		defaultViewport: chromium.defaultViewport,
